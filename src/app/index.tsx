16665454df7cbc4e1aaa2f8b6cb5585f380b96a4
YasOlBatista <yasmim.o.batista@aluno.senai.br>
2026-09-01 13:40:28 -0300

    iniciando exercicio

---
+++ src/app/index.tsx
@@ -0,0 +1,43 @@
+import { TouchableOpacity, Text, StyleSheet, ImageBackground} from "react-native";
+import { router } from "expo-router";
+
+export default function Index() {
+
+    function signIn() {
+        router.navigate("/signIn");
+    }
+
+    return (
+        <ImageBackground source={require('@/assets/friends.jpg')} style={styles.container} resizeMode="cover">
+            <TouchableOpacity 
+                style={styles.button} 
+                activeOpacity={0.7}
+                onPress={signIn}
+            >
+                <Text style={styles.tittle}>Ver episódios</Text>
+            </TouchableOpacity>
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {   
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 32,
+    },
+    button: {
+        backgroundColor: '#6b0a2d',
+        borderRadius: 10,
+        paddingVertical: 20,
+        paddingHorizontal: 20
+    },
+    tittle:{
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#fbffde'
+    },
+
+})
+
---
+++ src/app/signIn.tsx
@@ -0,0 +1,43 @@
+import {View, TouchableOpacity, Text, StyleSheet} from "react-native";
+import { router } from "expo-router";
+
+export default function signIn() {
+    function back(){
+        router.back();
+    }
+
+    return (
+        <View style={styles.container}>
+        <TouchableOpacity 
+            style={styles.button} 
+            activeOpacity={0.7} 
+            onPress={back}
+        >
+            <Text style={styles.button} onPress={back}>
+                Voltar
+            </Text>
+
+        </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {   
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 32,
+        backgroundColor: '#6b0a2d'
+    },
+    button: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        fontWeight: 'bold',
+        color: '#6b0a2d',
+        fontSize: 18
+    }
+})
+
